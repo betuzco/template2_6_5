2f89a6207dae53a37f07c80c695440f495d701b4
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-27 10:15:39 -0600

    Se agregó los métodos destapar letra y así mismo se convirtio en lower case

--- lib/adivina-la-palabra.rb
+++ lib/adivina-la-palabra.rb
@@ -1,16 +1,18 @@
 class AdivinaLaPalabra
   
     def ocultar_palabra(palabra)
-        @palabraAAdivinar = palabra
+        @palabraAAdivinar = palabra.downcase
         @palabraOculta = "*" *palabra.size
         return @palabraOculta
     end 
 
     def destaparLetra (letra)
+        letra = letra.downcase
         (0..@palabraAAdivinar.size).each do |index|
-            if(index == letra)
+            if(@palabraAAdivinar[index] == letra)
+                @palabraOculta[index] = letra
             end
         end
-        return "****a****a"
+        return @palabraOculta
     end
 end
